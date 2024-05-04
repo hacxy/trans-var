09860b7da61e43060766145a59241f78ee857c58
Hacxy <hacxy.97@outlook.com>
2024-05-04 15:40:30 +0800

    fix: 调整变量输出结果, 提供四种不同格式

--- packages/trans-var-cli/src/index.ts
+++ packages/trans-var-cli/src/index.ts
@@ -4,6 +4,7 @@ import prompts from "prompts";
 import ora from "ora";
 import { getVarByCiba } from "trans-var";
 import clipboardy from "clipboardy";
+
 const bootstrap = async () => {
   const promptsResult = await prompts(
     {
@@ -20,13 +21,36 @@ const bootstrap = async () => {
   );
 
   const spinner = ora("Loading ...").start();
-  const varText = await getVarByCiba(promptsResult.name);
+  const result = await getVarByCiba(promptsResult.name);
 
-  if (varText) {
-    clipboardy.writeSync(varText);
-    spinner.succeed(varText + chalk.dim(" 已复制到剪贴板"));
+  spinner.succeed("获取变量成功");
+
+  if (result) {
+    const finalVar = await prompts(
+      {
+        type: "select",
+        name: "value",
+        message: "请选择变量:",
+        choices: Object.values(result).map((value: string) => {
+          return {
+            title: value,
+            value: value,
+          };
+        }),
+      },
+      {
+        onCancel() {
+          // 取消选择
+          console.log(chalk.red("❌ 操作取消"));
+          process.exit(1);
+        },
+      }
+    );
+    clipboardy.writeSync(finalVar.value);
+    console.log();
+    console.log(finalVar.value + chalk.dim(" 已复制到剪贴板"));
   } else {
-    spinner.fail("获取变量失败");
+    spinner.warn("无法翻译此变量");
   }
 };
 
--- packages/trans-var/src/index.ts
+++ packages/trans-var/src/index.ts
@@ -1,19 +1,38 @@
-import { formatSmallHump } from 'tianjie';
+import {
+  formatSmallHump,
+  formatPascalCase,
+  formatUpperCaseWithUnderscore,
+  formatLowerCaseWithUnderscore,
+  containsEnglish
+} from 'tianjie';
 /**
  * @name 通过金山词霸翻译变量
  * @param text 需要翻译的文本
  * @returns
  */
 export const getVarByCiba = async (text: string) => {
-  const request = await fetch(`https://ciba-service.hacxy.cn?text=${text}`);
-  const result = await request.json().then((res) => res.data);
+  let varText: string | undefined = '';
+  if (!containsEnglish(text)) {
+    const request = await fetch(`https://ciba-service.hacxy.cn?text=${text}`);
+    const result = await request.json().then((res) => res.data);
 
-  let varText: string | undefined = result?.symbols?.[0]?.parts?.[0]?.means?.[0]?.word_mean;
+    varText = result?.symbols?.[0]?.parts?.[0]?.means?.[0]?.word_mean;
+  } else {
+    varText = text;
+  }
 
   if (varText) {
     varText = varText.split(';')[0];
+    varText = varText.replace(/['"]/g, '');
     varText = varText.replace(/\[.*?\]/g, '');
-    varText = formatSmallHump(varText);
   }
-  return varText;
+
+  if (varText) {
+    return {
+      smallHump: formatSmallHump(varText || ''),
+      bigHump: formatPascalCase(varText || ''),
+      uppercaseSnakelike: formatUpperCaseWithUnderscore(varText || ''),
+      lowercaseSnakelike: formatLowerCaseWithUnderscore(varText || '')
+    };
+  }
 };
